d68f0cfd2d91bb5b54e2449f59793eaf259edafa
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-05-24 22:35:17 +0300

    new

--- Lesson2homework1/src/Main.java
+++ Lesson2homework1/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+                double fuelPerLiter = 1.2;
+                int distanceToCity = 120;
+                double fuelConsumption100Km = 8;
+
+
+                double fuelNeeded = (fuelConsumption100Km / 100) * distanceToCity;
+
+
+                double totalCost = fuelNeeded * fuelPerLiter;
+
+
+                System.out.println(totalCost + " $ The cost of travel ");
+            }
+        }
+
+
