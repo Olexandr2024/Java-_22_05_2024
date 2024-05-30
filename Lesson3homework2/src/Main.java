1afe63618509819eb5eede41330769d84222a0d6
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-05-30 17:23:38 +0300

    new

--- Lesson3homework2/src/Main.java
+++ Lesson3homework2/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Введите номер квартиры:");
+        int apartmentNumber = sc.nextInt();
+
+
+        final int FLOORS = 9;
+        final int APARTMENTS_PER_FLOOR = 4;
+        final int ENTRANCES = 5;
+        final int APARTMENTS_PER_ENTRANCE = FLOORS * APARTMENTS_PER_FLOOR;
+
+
+        int totalApartments = APARTMENTS_PER_ENTRANCE * ENTRANCES;
+
+        if (apartmentNumber < 1 || apartmentNumber > totalApartments) {
+            System.out.println("Такой квартиры нет в этом доме.");
+        } else {
+
+            int entranceNumber = (apartmentNumber - 1) / APARTMENTS_PER_ENTRANCE + 1;
+
+
+            int apartmentInEntrance = (apartmentNumber - 1) % APARTMENTS_PER_ENTRANCE;
+            int floorNumber = apartmentInEntrance / APARTMENTS_PER_FLOOR + 1;
+
+
+            System.out.println("Квартира " + apartmentNumber + " находится в подъезде " + entranceNumber + ", на " + floorNumber + " этаже.");
+        }
+
     }
 }
