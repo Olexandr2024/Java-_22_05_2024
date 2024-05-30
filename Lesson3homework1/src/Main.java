edc8f79b764eb631c95965e41daa14165c04b5c9
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-05-30 17:17:55 +0300

    new

--- Lesson3homework1/src/Main.java
+++ Lesson3homework1/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int number1, number2, number3, number4, maxNumber;
+
+
+        System.out.println("Введите число 1:");
+        number1 = sc.nextInt();
+        System.out.println("Введите число 2:");
+        number2 = sc.nextInt();
+        System.out.println("Введите число 3:");
+        number3 = sc.nextInt();
+        System.out.println("Введите число 4:");
+        number4 = sc.nextInt();
+
+
+        maxNumber = number1;
+
+        if (number2 > maxNumber) {
+            maxNumber = number2;
+        }
+        if (number3 > maxNumber) {
+            maxNumber = number3;
+        }
+        if (number4 > maxNumber) {
+            maxNumber = number4;
+        }
+
+
+        System.out.println("Самое большое число = " + maxNumber);
     }
 }
