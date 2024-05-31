1712e12d668b7bff6780b0c83c3ebdcbf23fb948
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-05-31 11:39:01 +0300

    new

--- Lesson3homework3/src/Main.java
+++ Lesson3homework3/src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     Scanner sc = new Scanner(System.in);
+
+     System.out.println("Введите длину стороны a:");
+     int a = sc.nextInt();
+     System.out.println("Введите длину стороны b:");
+     int b = sc.nextInt();
+     System.out.println("Введите длину стороны с:");
+     int c = sc.nextInt();
+
+     if ((a + b > c) && (a + c > b) && (b + c > a)) {
+         System.out.println("Треугольник с такими сторонами существует.");
+     } else {
+         System.out.println("Треугольник с такими сторонами не существует.");
+     }
+
     }
 }
