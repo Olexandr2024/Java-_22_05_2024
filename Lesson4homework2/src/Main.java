6c71bfcc7d8939e114c6cc0ce66f6936205ef287
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-06-02 17:33:48 +0300

    new

--- Lesson4homework2/src/Main.java
+++ Lesson4homework2/src/Main.java
@@ -6,24 +6,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
 
-        System.out.println("Введите высоту прямоугольника:");
-        int height = sc.nextInt();
-        System.out.println("Введите ширину прямоугольника:");
-        int width = sc.nextInt();
+        System.out.println("Введите число для таблицы умножения:");
+        int number = sc.nextInt();
 
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-
-                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " x " + number + " = " + (i * number));
         }
 
-
+        sc.close();
     }
 }
