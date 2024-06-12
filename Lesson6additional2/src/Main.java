4550642b62d93607be27818e2c4b8699f82e778c
Olexandr2024 <stepanenko.aleksandr@fmail.com>
2024-06-12 19:25:52 +0300

    new

--- Lesson6additional2/src/Main.java
+++ Lesson6additional2/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input text: ");
+        String text = sc.nextLine();
+
+        // Нахождение самого короткого слова
+        String shortestWord = findShortestWord(text);
+
+        System.out.println("Вовочка писал слово - «" + shortestWord + "»");
+    }
+
+    public static String findShortestWord(String text) {
+        int length = text.length();
+
+        for (int wordLength = 1; wordLength <= length; wordLength++) {
+            String candidate = text.substring(0, wordLength);
+            if (text.replace(candidate, "").isEmpty()) {
+                return candidate;
+            }
+        }
+        return text;  // Если не нашлось, возвращаем исходный текст (хотя такого быть не должно)
     }
 }
